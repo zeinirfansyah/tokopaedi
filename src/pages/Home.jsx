9fbd65d3370767550dedab0ac5dd70e1cf888673
zeinirfansyah <12.zeinirfansyah@gmail.com>
2024-05-01 14:30:27 +0700

    refactor [home]: fixed bug by added filtered product state

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,39 +1,42 @@
 import useStore from "../stores/store";
 import Product from "../components/products/Product";
 import Filter from "../components/ui/Filter";
-import Layout from "../layouts/Layout";
+import { useEffect } from "react";
 
 const Home = () => {
   const store = useStore();
+  const filteredProducts = store.getFilteredProducts();
+
+  useEffect(() => {
+    store.fetchProducts();
+  }, [store]);
+
 
   const handleFilterProducts = (e) => {
     store.filterProducts(e.target.value);
   };
 
-  const filteredProducts = store.getFilteredProducts();
 
   return (
-    <Layout>
-      <section className="max-w-7xl mx-auto px-4 py-4">
-        <div>
-          <Filter
-            options={store.categories}
-            value={store.selectedCategory}
-            onChange={handleFilterProducts}
+    <section className="max-w-7xl mx-auto px-4 py-4">
+      <div>
+        <Filter
+          options={store.categories}
+          value={store.selectedCategory}
+          onChange={handleFilterProducts}
+        />
+      </div>
+      <div className="grid grid-cols-2 lg:grid-cols-6 gap-2 py-4">
+        {filteredProducts.map((product) => (
+          <Product
+            key={product.id}
+            product={product}
+            onAddToCart={() => store.addToCart(product)}
+            onProductDetails={() => store.ProductDetails(product)}
           />
-        </div>
-        <div className="grid grid-cols-2 lg:grid-cols-6 gap-2 py-4">
-          {filteredProducts.map((product) => (
-            <Product
-              key={product.id}
-              product={product}
-              onAddToCart={() => store.addToCart(product)}
-              onProductDetails={() => store.ProductDetails(product)}
-            />
-          ))}
-        </div>
-      </section>
-    </Layout>
+        ))}
+      </div>
+    </section>
   );
 };
 
