691c17c63a43ccb081a82bfad74fbc93bccb2d78
zeinirfansyah <12.zeinirfansyah@gmail.com>
2024-05-01 11:52:26 +0700

    refactor [button]: changed props

--- src/components/ui/Button.jsx
+++ src/components/ui/Button.jsx
@@ -1,8 +1,8 @@
 import { PropTypes } from 'prop-types';
 
-export const Button = (props) => {
+export const Button = ({onClick, style, children,}) => {
   return (
-    <button onClick={props.onClick} className={`px-4 py-2 border border-green-600 rounded-lg text-[12px] font-semibold ${props.color}`}>{props.children}</button>
+    <button onClick={onClick} className={`px-4 py-2 border border-green-600 rounded-lg text-[12px] font-semibold ${style}`}>{children}</button>
   )
 }
 
@@ -10,4 +10,5 @@ Button.propTypes = {
   onClick: PropTypes.func,
   color: PropTypes.string,
   children: PropTypes.node.isRequired,
+  style: PropTypes.string
 }
