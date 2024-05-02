e606e0ce295e9b1c6bdfef41d31edd52c97d17c9
zeinirfansyah <12.zeinirfansyah@gmail.com>
2024-05-02 17:57:40 +0700

    refactor [CheckoutHistoryDetail.jsx]: added style

--- src/pages/CheckoutHistoryDetail.jsx
+++ src/pages/CheckoutHistoryDetail.jsx
@@ -1,5 +1,7 @@
 import { useParams } from "react-router-dom";
 import useStore from "../stores/store";
+import LinesEllipsis from "react-lines-ellipsis";
+import star from "../assets/icons/star.svg";
 
 const CheckoutHistoryDetail = () => {
   const { id } = useParams();
@@ -14,9 +16,36 @@ const CheckoutHistoryDetail = () => {
     <>
       <div className="max-w-3xl mx-auto px-4">
         {checkout.products.map((product) => (
-          <div key={product.id}>
-            <h2>{product.title}</h2>
-            <p>{product.price}</p>
+          <div key={product.id} className="flex gap-6 py-2 px-2 border">
+            <img
+              src={product.image}
+              alt={product.title}
+              className="w-20 h-20 object-contain"
+            />
+            <div className="flex flex-col gap-1 w-full">
+              <LinesEllipsis
+                text={product.title}
+                maxLine="2"
+                ellipsis=" ... "
+                trimRight
+                basedOn="words"
+                className="text-[14px] tracking-tight text-gray-600"
+              />
+              <p className="text-[14px]">{product.price} x {product.number}</p>
+              <p className="text-[14px] font-bold">
+                $ {(product.price * product.number).toFixed(2)}
+              </p>
+              <div className="flex justify-between">
+                <div className="flex gap-1 items-center text-xs tracking-tight">
+                  <span className="flex gap-1">
+                    <img src={star} alt="" />
+                    {product.rating}
+                  </span>
+                  <span>|</span>
+                  <span>{product.count}+ terjual</span>
+                </div>
+              </div>
+            </div>
           </div>
         ))}
       </div>
