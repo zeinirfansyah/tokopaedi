5b04c66fc010193a8bbca9b576815d5f69a486a8
zeinirfansyah <12.zeinirfansyah@gmail.com>
2024-05-02 12:01:26 +0700

    refactor [navbar]: added route to history

--- src/components/header/Header.jsx
+++ src/components/header/Header.jsx
@@ -12,20 +12,30 @@ const Header = () => {
     <>
       <div className="max-w-7xl mx-auto px-4">
         <div className="flex items-center justify-between gap-4">
-          <Link to="/" className="text-3xl font-semibold text-green-600 hidden lg:block">
+          <Link
+            to="/"
+            className="text-3xl font-semibold text-green-600 hidden lg:block"
+          >
             tokopaedi
           </Link>
           <div className="w-full">
-          <SearchInput type="text" placeholder="Cari di Tokopaedi (Fitur ini masih pajangan saja)" />
+            <SearchInput
+              type="text"
+              placeholder="Cari di Tokopaedi (Fitur ini masih pajangan saja)"
+            />
           </div>
           <div className="flex flex-row items-center gap-2">
-            <img src={envelope} alt="" className="w-8" />
+            <Link to="/history">
+              <img src={envelope} alt="" className="w-8 hover:cursor-pointer" />
+            </Link>
             <Link to="/cart">
-              <span className="absolute ms-2 -mt-3">
-                <div className="inline-flex items-center px-1.5 py-0.5 rounded-full text-[8pt] font-semibold bg-red-500 text-white">
-                  {store.count}
-                </div>
-              </span>
+              {store.count > 0 && (
+                <span className="absolute ms-2 -mt-3">
+                  <div className="inline-flex items-center px-1.5 py-0.5 rounded-full text-[8pt] font-semibold bg-red-500 text-white">
+                    {store.count}
+                  </div>
+                </span>
+              )}
               <img src={cart} alt="" className="w-8 hover:cursor-pointer" />
             </Link>
             <img src={more} alt="" className="w-8" />
